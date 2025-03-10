38d4760d01b6686fe1d1d4fe497051b62a1927d6
Sunnyliu2025 <2277428036@qq.com>
2025-03-10 18:24:11 +0800

    bug fix

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const webpack = require("webpack");
 
 const urlDev = "https://localhost:3000/";
-const urlProd = "https://github.com/Sunnyliu2025/WordGPT/blob/main//"; // CHANGE THIS TO YOUR PRODUCTION DEPLOYMENT LOCATION
+const urlProd = "https://sunnyliu2025.github.io/WordGPT/"; // CHANGE THIS TO YOUR PRODUCTION DEPLOYMENT LOCATION
 
 async function getHttpsOptions() {
   const httpsOptions = await devCerts.getHttpsServerOptions();
