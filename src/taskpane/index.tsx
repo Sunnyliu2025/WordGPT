ceada767766b264d6b3e65e50ae32ce9e5f20112
Sunnyliu2025 <2277428036@qq.com>
2026-07-10 15:40:41 +0800

    fix

--- src/taskpane/index.tsx
+++ src/taskpane/index.tsx
@@ -1,15 +1,18 @@
 import "./taskpane.css";
 import App from "./components/App";
+import { AppContainer } from "react-hot-loader";
 import { ThemeProvider } from "@fluentui/react";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-/* global Office, module, require, process */
+/* global Office, module, require */
 
 const render = (Component) => {
   ReactDOM.render(
-    <ThemeProvider>
-      <Component />
-    </ThemeProvider>,
+    <AppContainer>
+      <ThemeProvider>
+        <Component />
+      </ThemeProvider>
+    </AppContainer>,
     // eslint-disable-next-line no-undef
     document.getElementById("container")
   );
@@ -19,8 +22,7 @@ Office.onReady(() => {
   render(App);
 });
 
-// 开发环境热更新支持，生产环境自动移除
-if (process.env.NODE_ENV !== "production" && (module as any).hot) {
+if ((module as any).hot) {
   (module as any).hot.accept("./components/App", () => {
     const NextApp = require("./components/App").default;
     render(NextApp);
--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,6 @@ const devCerts = require("office-addin-dev-certs");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
-const TerserPlugin = require("terser-webpack-plugin");
 const webpack = require("webpack");
 
 const urlDev = "https://localhost:3000/";
@@ -19,69 +17,18 @@ async function getHttpsOptions() {
 module.exports = async (env, options) => {
   const dev = options.mode === "development";
   const config = {
-    // 生产环境不生成 source-map，开发环境使用轻量级 eval-source-map
-    devtool: dev ? "eval-source-map" : false,
+    devtool: "source-map",
     entry: {
       polyfill: ["core-js/stable", "regenerator-runtime/runtime"],
-      // 注意：不再手动声明 vendor entry，改由 splitChunks 自动提取第三方依赖
-      // react-hot-loader/patch 仅在开发环境需要
-      taskpane: [
-        ...(dev ? ["react-hot-loader/patch"] : []),
-        "./src/taskpane/index.tsx",
-        "./src/taskpane/taskpane.html",
-      ],
+      vendor: ["react", "react-dom", "core-js", "@fluentui/react"],
+      taskpane: ["react-hot-loader/patch", "./src/taskpane/index.tsx", "./src/taskpane/taskpane.html"],
       commands: "./src/commands/commands.ts",
     },
     output: {
       clean: true,
-      filename: dev ? "[name].js" : "[name].[contenthash:8].js",
-      chunkFilename: dev ? "[name].js" : "[name].[contenthash:8].js",
     },
     resolve: {
       extensions: [".ts", ".tsx", ".html", ".js"],
-      // 开发环境使用 react-hot-loader 的 react-dom 以支持 HMR
-      ...(dev && {
-        alias: {
-          "react-dom": "@hot-loader/react-dom",
-        },
-      }),
-    },
-    optimization: {
-      // 关键：提取所有第三方依赖到 vendor chunk，避免重复打包
-      splitChunks: {
-        chunks: "all",
-        cacheGroups: {
-          vendor: {
-            name: "vendor",
-            test: /[\\/]node_modules[\\/]/,
-            chunks: "all",
-            priority: 10,
-          },
-          // Fluent UI 体积较大，单独拆分为 fluentui chunk，可独立缓存
-          fluentui: {
-            name: "fluentui",
-            test: /[\\/]node_modules[\\/]@fluentui[\\/]/,
-            chunks: "all",
-            priority: 20,
-            reuseExistingChunk: true,
-          },
-        },
-      },
-      minimizer: [
-        new TerserPlugin({
-          terserOptions: {
-            compress: {
-              drop_console: !dev, // 生产环境移除 console.log
-              drop_debugger: true,
-            },
-            output: {
-              comments: false, // 移除注释
-            },
-          },
-          extractComments: false, // 不提取 LICENSE 文件
-        }),
-        new CssMinimizerPlugin(), // 压缩 CSS
-      ],
     },
     module: {
       rules: [
@@ -98,36 +45,16 @@ module.exports = async (env, options) => {
         {
           test: /\.tsx?$/,
           exclude: /node_modules/,
-          use: [...(dev ? ["react-hot-loader/webpack"] : []), "ts-loader"],
+          use: ["react-hot-loader/webpack", "ts-loader"],
         },
         {
           test: /\.css$/,
-          use: [
-            MiniCssExtractPlugin.loader,
-            {
-              loader: "css-loader",
-              options: {
-                // 生产环境启用 CSS 压缩
-                ...(!dev && {
-                  importLoaders: 0,
-                }),
-              },
-            },
-          ],
+          use: [MiniCssExtractPlugin.loader, "css-loader"],
         },
         {
           test: /\.html$/,
           exclude: /node_modules/,
-          use: [
-            {
-              loader: "html-loader",
-              options: {
-                // 生产环境压缩 HTML
-                minimize: !dev,
-                sources: false, // 不处理 HTML 中的资源引用（由 webpack 处理）
-              },
-            },
-          ],
+          use: "html-loader",
         },
         {
           test: /\.(png|jpg|jpeg|gif|ico)$/,
@@ -166,33 +93,17 @@ module.exports = async (env, options) => {
         ],
       }),
       new MiniCssExtractPlugin({
-        filename: dev ? "[name].css" : "[name].[contenthash:8].css",
-        chunkFilename: dev ? "[name].css" : "[name].[contenthash:8].css",
+        filename: "[name].[contenthash].css",
       }),
       new HtmlWebpackPlugin({
         filename: "taskpane.html",
         template: "./src/taskpane/taskpane.html",
-        chunks: ["taskpane", "vendor", "fluentui", "polyfill"],
-        // 生产环境压缩 HTML
-        ...(!dev && {
-          minify: {
-            removeComments: true,
-            collapseWhitespace: true,
-            removeAttributeQuotes: true,
-          },
-        }),
+        chunks: ["taskpane", "vendor", "polyfills"],
       }),
       new HtmlWebpackPlugin({
         filename: "commands.html",
         template: "./src/commands/commands.html",
         chunks: ["commands"],
-        ...(!dev && {
-          minify: {
-            removeComments: true,
-            collapseWhitespace: true,
-            removeAttributeQuotes: true,
-          },
-        }),
       }),
       new webpack.ProvidePlugin({
         Promise: ["es6-promise", "Promise"],
